0fcbc1df004b8dba0aaf524cb3bd25ca5d375506
eugenebaek <eugenebaek5100@gmail.com>
2021-09-25 03:43:59 -0700

    home page

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,51 @@
 import Head from 'next/head'
+import clientPromise from "../lib/mongodb";
+import Date from "../components/date";
 
-export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Eugene Baek</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+export default function Home({home}) {
+    return (
+        <div className="container">
+            <Head>
+                <title>Eugene Baek</title>
+                <link rel="icon" href="/favicon.ico"/>
+            </Head>
 
-      <main>
+            <main>
+                {home.map((home) => (
+                    <div key={home.id} className='mb-16'>
+                        <div>
+                            {home.images.map(image => <img className='my-5' src={image}/>)}
+                        </div>
+                        <div className='flex my-8'>
+                            <div className='flex-1'>
+                                <h2>{home.title}</h2>
+                                <Date dateString={home.date}/>
+                                <h2>{home.subtitle}</h2>
+                            </div>
+                            <div className='flex-1'>
+                                <p>{home.message}</p>
+                            </div>
+                        </div>
+                    </div>
+                ))}
 
-      </main>
-    </div>
-  )
+            </main>
+        </div>
+    )
+}
+
+export async function getServerSideProps(context) {
+    const client = await clientPromise
+    const db = await client.db('portfolio');
+
+    const home = await db
+        .collection("home")
+        .find({})
+        .toArray();
+
+    return {
+        props: {
+            home: JSON.parse(JSON.stringify(home))
+        }
+    }
 }
