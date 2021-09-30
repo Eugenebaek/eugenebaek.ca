06bbab06f8ce89adfe6f67e1fe7af907cf65892f
eugenebaek <eugenebaek5100@gmail.com>
2021-09-29 20:04:36 -0700

    fixed about width

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,14 +1,13 @@
 export default function About() {
     return (
-        <div className="container">
             <main>
                 <h1>Eugene Baek</h1>
                 <h2>Technology Co-op @ Symcor</h2>
                 <h2>University of Waterloo/Computer Engineering Student</h2>
 
                 <p>Eugene Baek is a 2nd year Computer Engineering student at the University of Waterloo. </p>
-                <div className='whitespace-pre-wrap font-RobotoMono'>His passions are in game development and machine learning. To pursue these passions, he uses C++ to
-                    create Neural Networks and work with SFML to render projects.</div>
+                <p>His passions are in game development and machine learning. To pursue these passions, he uses C++ to
+                    create Neural Networks and work with SFML to render projects.</p>
                 <p>In his first year, Eugene had the opportunity to work at Fidelity Canada as an Automation Specialist. As
                     part of the Quality Assurance team, he was tasked to write automation scripts in Groovy and Java to
                     verify the functionality of various webpage components and database interactions.</p>
@@ -19,6 +18,5 @@ export default function About() {
                 <p>2020-2021</p>
                 <p>2019-2020</p>
             </main>
-        </div>
     )
 }
