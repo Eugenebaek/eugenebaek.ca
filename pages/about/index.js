e0693280d9326e310146bb7026233a42652fdb3b
eugenebaek <eugenebaek5100@gmail.com>
2021-10-01 03:20:43 -0700

    design changes

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,32 +1,38 @@
 export default function About() {
 
-    const experiences = ["Fidelity Canada - Automation Specialist", "Symcor - Technology Co-op"]
-    const experienceList = experiences.map((experience) => <p>{experience}</p>)
+    const experiences = [
+        {title: "Technology Co-op", company: "Symcor", date: "July 2021 - Feb 2021"},
+        {title: "Automation Specialist", company: "Fidelity Canada", date: "Jan 2020 - May 2020"},
+        {title: "Head Systems Administrator", company: "Seung Oh Ltd", date: "Sept 2017 - Dec 2020"}
+    ]
 
     return (
-        <main className='w-1/2'>
-            <div className='mb-10'>
+        <main className='w-full sm:w-1/2'>
+            <div className='mb-16 font-normal'>
                 <h1>Eugene Baek</h1>
                 <h2>Technology Co-op @ Symcor</h2>
                 <h2>University of Waterloo/Computer Engineering Student</h2>
             </div>
-            <div>
-                <p>Eugene Baek is a 2nd year Computer Engineering student at the University of Waterloo. </p>
-                <p>His passions are in game development and machine learning. To pursue these passions, he uses C++ to
+            <div className='mb-16'>
+                <p className='mb-4'>Eugene Baek is a 2nd year Computer Engineering student at the University of
+                    Waterloo. His passions are in game development and machine learning. To pursue these passions, he
+                    uses C++ to
                     create Neural Networks and work with SFML to render projects.</p>
-                <p>In his first year, Eugene had the opportunity to work at Fidelity Canada as an Automation Specialist.
-                    As
-                    part of the Quality Assurance team, he was tasked to write automation scripts in Groovy and Java to
+                <p className='mb-4'>In his first year, Eugene had the opportunity to work at Fidelity Canada as an
+                    Automation Specialist.
+                    As part of the Quality Assurance team, he was tasked to write automation scripts in Groovy and Java
+                    to
                     verify the functionality of various webpage components and database interactions.</p>
                 <p>Some of Eugene's hobbies include Hockey, League of Legends, and Photography. He is currently a Silver
                     4
                     Katarina main.</p>
             </div>
-
-
             <div>
-                <h2>Work Experience</h2>
-                {experienceList}
+                <h2 className='mb-2 font-normal'>Work Experience</h2>
+                {experiences.map((experience) => (
+                    <p><span className='underline'>{experience.date}</span> {experience.title} @ {experience.company}
+                    </p>
+                ))}
             </div>
         </main>
     )
