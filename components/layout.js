0090e8ea2d27200faa83bf9ea36f5977e0cc67db
eugenebaek <eugenebaek5100@gmail.com>
2021-10-20 00:32:18 -0700

    added sidebar content

--- components/layout.js
+++ components/layout.js
@@ -1,9 +1,12 @@
 import Head from 'next/head'
 import React from "react";
 import Link from "next/link";
+import {misc, music, movies, socials} from "../public/sidebar_content";
 
 export default function Layout({children}) {
     const [sidebarOpen, setSidebarOpen] = React.useState(false);
+
+    const movies_and_shows = [];
 
     return (
         <>
@@ -12,18 +15,19 @@ export default function Layout({children}) {
                 <link rel="icon" href="/favicon.ico"/>
             </Head>
 
-            <div className='font-Roboto font-extralight text-xs sm:text-sm'>
+            <div className='font-Roboto font-light text-xs'>
                 <div className='flex gap-2 mx-2 sm:gap-12 sm:mx-6'>
                     <div className='w-full md:w-3/4 pt-44'>{children}</div>
                     <div className='hidden md:flex w-1/4 h-screen sticky top-0'>
                         <Sidebar/>
                     </div>
-                    <button onClick={() => setSidebarOpen(!sidebarOpen)} className="md:hidden menu-button fixed top-4 right-2 z-50">
+                    <button onClick={() => setSidebarOpen(!sidebarOpen)}
+                            className="md:hidden menu-button fixed top-4 right-2 z-50">
                         {(sidebarOpen) ? '< close >' : '< open >'}
                     </button>
                     {(sidebarOpen)
-                    ? <MobileSidebar/>
-                    : ''}
+                        ? <MobileSidebar/>
+                        : ''}
                 </div>
             </div>
         </>
@@ -32,31 +36,73 @@ export default function Layout({children}) {
 
 function MobileSidebar() {
     return (
-        <nav className='fixed w-full h-screen bg-white text-base'>
-            <div className='fixed py-20 uppercase'>
-                <Link href="/">
-                    <a onClick={() => setSidebarOpen(!sidebarOpen)}>Eugene Baek</a>
-                </Link>
-            </div>
-            <div className='flex flex-col pt-44 gap-y-1.5'>
-                <Link href="/about">
-                    <a onClick={() => setSidebarOpen(!sidebarOpen)}>About</a>
-                </Link>
-                <Link href="/portfolio">
-                    <a onClick={() => setSidebarOpen(!sidebarOpen)}>Portfolio</a>
-                </Link>
-                <Link href="/blog">
-                    <a onClick={() => setSidebarOpen(!sidebarOpen)}>Blog</a>
-                </Link>
+        <nav className='fixed w-screen h-screen bg-white text-sm'>
+            <div className="my-4 mx-2">
+                <div className='uppercase'>
+                    <Link href="/">
+                        <a onClick={() => setSidebarOpen(!sidebarOpen)}>Eugene Baek</a>
+                    </Link>
+                </div>
+                <div className='flex flex-col divide-y pt-6 gap-y-1.5'>
+                    <Link href="/about">
+                        <a onClick={() => setSidebarOpen(!sidebarOpen)}>About</a>
+                    </Link>
+                    <Link href="/portfolio">
+                        <a onClick={() => setSidebarOpen(!sidebarOpen)}>Portfolio</a>
+                    </Link>
+                    <Link href="/blog">
+                        <a onClick={() => setSidebarOpen(!sidebarOpen)}>Blog</a>
+                    </Link>
+                </div>
+                <div className="text-xs">
+                    <div className='flex flex-col pt-4 gap-y-0.5'>
+                        {misc.map((misc) => (
+                            <Link href={misc.link}>
+                                <a target="_blank">{misc.message}</a>
+                            </Link>
+                        ))}
+                    </div>
+                    <div className='flex flex-col pt-4 gap-y-0.5'>
+                        <h1>~music~</h1>
+                        {music.map((music) => (
+                            <Link href={music.link}>
+                                <a target="_blank">{music.title} - {music.artist}</a>
+                            </Link>
+                        ))}
+                    </div>
+                    <div className='flex flex-col pt-4 gap-y-0.5'>
+                        <h1>~movies/shows~</h1>
+                        {movies.map((movie) => (
+                            <Link href={movie.link}>
+                                <a target="_blank">{movie.title} - {movie.director}</a>
+                            </Link>
+                        ))}
+                    </div>
+                    <div className='flex flex-row pt-4 gap-y-0.5'>
+                        <div className="flex-1">
+                            <h1>ᕦ(ˇò_ó)ᕤ</h1>
+                        </div>
+                        <div className="flex-1">
+                            <div className="flex flex-col">
+                                {socials.map((social) => (
+                                    <Link href={social.link}>
+                                        <a target="_blank">> {social.title}</a>
+                                    </Link>
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </nav>
     )
 }
 
 function Sidebar() {
+
     return (
         <nav className=''>
-            <div className='fixed py-20 uppercase'>
+            <div className='fixed py-20 uppercase text-sm'>
                 <Link href="/">
                     <a>Eugene Baek</a>
                 </Link>
@@ -71,6 +117,45 @@ function Sidebar() {
                 <Link href="/blog">
                     <a>Blog</a>
                 </Link>
+            </div>
+
+            <div className='flex flex-col pt-10 gap-y-0.5'>
+                {misc.map((misc) => (
+                    <Link href={misc.link}>
+                        <a target="_blank">{misc.message}</a>
+                    </Link>
+                ))}
+            </div>
+
+            <div className='flex flex-col pt-10 gap-y-0.5'>
+                <h1>~music~</h1>
+                {music.map((music) => (
+                    <Link href={music.link}>
+                        <a target="_blank">{music.title} - {music.artist}</a>
+                    </Link>
+                ))}
+            </div>
+            <div className='flex flex-col pt-10 gap-y-0.5'>
+                <h1>~movies/shows~</h1>
+                {movies.map((movie) => (
+                    <Link href={movie.link}>
+                        <a target="_blank">{movie.title} - {movie.director}</a>
+                    </Link>
+                ))}
+            </div>
+            <div className='flex flex-row pt-10 gap-y-0.5'>
+                <div className="flex-1">
+                    <h1>ᕦ(ˇò_ó)ᕤ</h1>
+                </div>
+                <div className="flex-1">
+                    <div className="flex flex-col">
+                        {socials.map((social) => (
+                            <Link href={social.link}>
+                                <a target="_blank">> {social.title}</a>
+                            </Link>
+                        ))}
+                    </div>
+                </div>
             </div>
         </nav>
     )
---
+++ public/sidebar_content.js
@@ -0,0 +1,28 @@
+export const misc = [
+    {message: "Brucia La Terra from The Godfather 3", link: "https://www.youtube.com/watch?v=mwMOvx8kvSc&ab_channel=%CC%AE%CC%88"},
+]
+
+export const music = [
+    {title: "Everything", artist: "The Black Skirts", link: "https://www.youtube.com/watch?v=Aq_gsctWHtQ&ab_channel=SEOULMUSIC%2F%EC%84%9C%EC%9A%B8%EB%AE%A4%EC%A7%81"},
+    {title: "Liebestraum No. 3", artist: "Franz Liszt", link: "https://www.youtube.com/watch?v=KpOtuoHL45Y&ab_channel=TheWickedNorth"},
+    {title: "눈(SNOW)", artist: "Zion.T (feat.이문세)", link: "https://www.youtube.com/watch?v=fiGSDywrX1Y&ab_channel=YGENTERTAINMENT"},
+    {title: "Tree(나무)", artist: "Car, the garden(카더가든)", link: "https://www.youtube.com/watch?v=cHkDZ1ekB9U&ab_channel=1theK%28%EC%9B%90%EB%8D%94%EC%BC%80%EC%9D%B4%29"},
+    {title: "Gymnopédie No.1", artist: "Erik Satie", link: "https://www.youtube.com/watch?v=S-Xm7s9eGxU&ab_channel=DistantMirrors"},
+    {title: "Me and Mrs. Jones", artist: "Billy Paul", link: "https://www.youtube.com/watch?v=JKEItVNlYXI&ab_channel=PhiladelphiaInternationalRecords"},
+    {title: "Les feuilles mortes", artist: "Yves Montand", link: "https://www.youtube.com/watch?v=wRe1U17n5OY&ab_channel=EdmundStAustell"},
+    {title: "Caruso", artist: "Luciano Pavarotti", link: "https://www.youtube.com/watch?v=I8A61eY1Efg&ab_channel=no%C3%A9%C3%A8on"},
+];
+
+export const movies = [
+    {title: "The Godfather", director: "Francis Ford Coppola", link: "https://www.imdb.com/title/tt0068646/?pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=e31d89dd-322d-4646-8962-327b42fe94b1&pf_rd_r=2FY2Q7MNK0QQX52GM01M&pf_rd_s=center-1&pf_rd_t=15506&pf_rd_i=top&ref_=chttp_tt_2"},
+    {title: "Schindler's List", director: "Steven Spielberg", link: "https://www.imdb.com/title/tt0108052/?pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=e31d89dd-322d-4646-8962-327b42fe94b1&pf_rd_r=2FY2Q7MNK0QQX52GM01M&pf_rd_s=center-1&pf_rd_t=15506&pf_rd_i=top&ref_=chttp_tt_6"},
+    {title: "Léon: The Professional", director: "Luc Besson", link: "https://www.imdb.com/title/tt0110413/?pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=e31d89dd-322d-4646-8962-327b42fe94b1&pf_rd_r=2FY2Q7MNK0QQX52GM01M&pf_rd_s=center-1&pf_rd_t=15506&pf_rd_i=top&ref_=chttp_tt_31"},
+    {title: "Good Will Hunting", director: "Gus Van Sant", link: "https://www.imdb.com/title/tt0119217/?pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=e31d89dd-322d-4646-8962-327b42fe94b1&pf_rd_r=2FY2Q7MNK0QQX52GM01M&pf_rd_s=center-1&pf_rd_t=15506&pf_rd_i=top&ref_=chttp_tt_86"},
+    {title: "Howl's Moving Castle", director: "Hayao Miyazaki", link: "https://www.imdb.com/title/tt0347149/?pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=e31d89dd-322d-4646-8962-327b42fe94b1&pf_rd_r=2FY2Q7MNK0QQX52GM01M&pf_rd_s=center-1&pf_rd_t=15506&pf_rd_i=top&ref_=chttp_tt_140"},
+]
+
+export const socials = [
+    {title: "email", link: "mailto:eugenebaek5100@gmail.com"},
+    {title: "linkedin", link: "https://www.linkedin.com/in/eugene-baek-601375194/"},
+    {title: "github", link: "https://github.com/Eugenebaek"},
+]
