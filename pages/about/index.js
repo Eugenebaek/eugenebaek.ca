62e3cec5a9cb02836c366fd132b329bb7db092ea
eugenebaek <eugenebaek5100@gmail.com>
2022-04-21 14:44:18 -0700

    updated about section

--- pages/about/index.js
+++ pages/about/index.js
@@ -10,22 +10,25 @@ export default function About() {
         <main className='w-full sm:w-1/2'>
             <div className='mb-16 font-normal'>
                 <h1>Eugene Baek</h1>
-                <h2>Technology Co-op @ Symcor</h2>
+                <h2>Spring 2022 - 2B Term</h2>
                 <h2>University of Waterloo/Computer Engineering Student</h2>
             </div>
             <div className='mb-16'>
                 <p className='mb-4'>Eugene Baek is a 2nd year Computer Engineering student at the University of
-                    Waterloo. His passions are in game development and machine learning. To pursue these passions, he
-                    uses C++ to
-                    create Neural Networks and work with SFML to render projects.</p>
+                    Waterloo. His passions are in software development and machine learning. To pursue these passions,
+                    he
+                    uses C++ and Python to
+                    create Neural Networks and work with TensorFlow.</p>
                 <p className='mb-4'>In his first year, Eugene had the opportunity to work at Fidelity Canada as an
                     Automation Specialist.
                     As part of the Quality Assurance team, he was tasked to write automation scripts in Groovy and Java
                     to
                     verify the functionality of various webpage components and database interactions.</p>
-                <p>Some of Eugene's hobbies include Hockey, League of Legends, and Photography. He is currently a Silver
-                    4
-                    Katarina main.</p>
+                <p className='mb-4'>In his second year, Eugene joined Symcor to develop and program automated workflows
+                    and streamline a process that was done manually before. Working in python, Eugene had created a data
+                    capture program that would generate powerpoints for quarterly review.</p>
+                <p>Some of Eugene's hobbies include Hockey (Vancouver Canucks), Movies, Music, and Photography. Check
+                    out my latest photos on my home page!</p>
             </div>
             <div>
                 <h2 className='mb-2 font-normal'>Work Experience</h2>
